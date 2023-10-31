70c1a33909b853ed35631087bfbe39c1b8ad03f8
AgosRobledo <131928579+AgosRobledo@users.noreply.github.com>
2023-10-30 23:02:57 -0300

    Add files via upload

--- Trivia.py
+++ Trivia.py
@@ -3,45 +3,37 @@ from tkinter import ttk
 import mysql.connector
 import random
 from tkinter import messagebox
-
+import time
 
 conexion = mysql.connector.connect(host="localhost", user="root", password="estudiantes2020", database="JUEGO", port=3305)
 
 cursor = conexion.cursor()
-pregunta_actual=0
+pregunta_actual = 0
 puntuacion = 0
-tiempo=0
+tiempo = 0
 
-root=None
-ventana=None
-
-
+root = None
+ventana = None
 
 def crear_ventana():
 
     def iniciar_juego():
-        global tiempo_transcurrido
-        tiempo_transcurrido=0
-        jugadores_data=[]
+
+        jugadores_data = []
         nombre = nombre_entry.get()
         apellido = apellido_entry.get()
         telefono = telefono_entry.get()
-        
+
         # Verificar si todos los campos están llenos
         if nombre and apellido and telefono:
-            jugadores_data.append((nombre, apellido, telefono))
+            jugadores_data.append((nombre, apellido, telefono, puntuacion))
             print(jugadores_data)
             ventana.destroy()
             abrir_juego()
 
-
-
-            #actualizar_treeview()
         else:
             messagebox.showerror("Error", "Los campos son obligatorios. Debes completarlos.")
 
-
-       
     def salir():
         ventana.destroy()
         root.destroy()
@@ -50,13 +42,12 @@ def crear_ventana():
     ventana.title("Jugadores")
     ventana.attributes('-fullscreen', True)
     ventana.configure(bg="#630551")
- 
 
     # Crear un frame con un borde visible para el formulario de inscripción
     formulario_frame = tk.Frame(ventana, bd=2, relief=tk.SOLID, bg="#F3C7AA")
     formulario_frame.grid(padx=10, pady=10)
 
-# Título del formulario
+    # Título del formulario
     titulo_label = tk.Label(formulario_frame, text="Jugador", bg="#F3C7AA", font=("Arial", 20, 'bold'))
     titulo_label.grid(row=0, column=0, columnspan=2, pady=10)
 
@@ -65,7 +56,7 @@ def crear_ventana():
     nombre_label.grid(row=1, column=0)
     nombre_entry = tk.Entry(formulario_frame)
     nombre_entry.grid(row=1, column=1, padx=5, pady=5, ipadx=5, ipady=5, sticky="ew")
-    
+
     apellido_label = tk.Label(formulario_frame, text="Apellido:", bg="#F3C7AA", font=("Arial", 15))
     apellido_label.grid(row=2, column=0)
     apellido_entry = tk.Entry(formulario_frame)
@@ -76,16 +67,16 @@ def crear_ventana():
     telefono_entry = tk.Entry(formulario_frame)
     telefono_entry.grid(row=3, column=1, padx=5, pady=5, ipadx=5, ipady=5, sticky="ew")
 
-    jugar_button = tk.Button(formulario_frame, text="JUGAR", bg="#9E5B00", font=("Arial",8, 'bold'), command=lambda:iniciar_juego())
+    jugar_button = tk.Button(formulario_frame, text="JUGAR", bg="#9E5B00", font=("Arial", 8, 'bold'), command=lambda:iniciar_juego())
     jugar_button.grid(row=6, columnspan=2, pady=1, sticky="ew")
 
-    jugar_button = tk.Button(formulario_frame, text="SALIR", bg="#9E5B00", font=("Arial",8, 'bold'), command=lambda:salir())
+    jugar_button = tk.Button(formulario_frame, text="SALIR", bg="#9E5B00", font=("Arial", 8, 'bold'), command=lambda:salir())
     jugar_button.grid(row=7, columnspan=3, pady=5, sticky="ew")
 
     titulo2_label = tk.Label(ventana, text="Tabla de Jugadores", font=("Arial", 35, 'bold'), bg="#630551", fg="white")
-    titulo2_label.grid(row=4, column=0, columnspan=5,  padx=250, pady=15)
-    #añadir=Button(ventana, text="Añadir", bg="#9E3700", fg="white"). grid(row=4, column=1, padx=20, pady=20)
-# Crear Treeview para mostrar la información
+    titulo2_label.grid(row=4, column=0, columnspan=5, padx=250, pady=15)
+
+    # Crear Treeview para mostrar la información
     tree = ttk.Treeview(ventana, columns=("idjugador", "Nombre", "Apellido", "telefono", "Puntaje", "Tiempo"))
     tree.heading("#2", text="Nombre")
     tree.heading("#3", text="Apellido")
@@ -95,14 +86,11 @@ def crear_ventana():
     tree.heading("#1", text="idjugador")
     tree.column("#1", width=0, stretch=tk.NO)
     tree.column("#0", width=0, stretch=tk.NO)  # Ocultar la columna #0 que habitualmente muestra las primary key de los objetos
-    tree.column("#2", width=230)#Aumenta el ancho del Treeview
-    tree.config(height=23) # Aumentar la altura del Treeview
+    tree.column("#2", width=230)  # Aumenta el ancho del Treeview
+    tree.config(height=23)  # Aumentar la altura del Treeview
     tree.grid(row=5, column=0, padx=250, pady=0)
+
 crear_ventana()
-
-
-
-
 
 root = tk.Tk()
 root.title("Juego de Preguntas y Respuestas")
@@ -116,74 +104,54 @@ frame_respuestas = tk.Frame(root, bg="#630551")
 frame_respuestas.pack()
 botones_respuesta = []
 
-
-
-
-
 def abrir_juego():
-        global root
-        
-        def obtener_preguntas():
-            cursor.execute("SELECT preguntas, respuestas, respuestaErronea_uno, respuestaErronea_dos, respuestaErronea_tres FROM Preguntas")
-            preguntas_respuestas = cursor.fetchall()
-            random.shuffle(preguntas_respuestas)  # Mezclar las preguntas para que aparezcan en orden aleatorio
-            return preguntas_respuestas
-        
-        preguntas_respuestas = obtener_preguntas()
-        
-        def mostrar_pregunta():
-            global pregunta_actual
-            if pregunta_actual < len(preguntas_respuestas):
-                pregunta, respuesta_correcta, respuesta_incorrecta_1, respuesta_incorrecta_2, respuesta_incorrecta_3 = preguntas_respuestas[pregunta_actual]
-                label_pregunta.config(text=pregunta)
-                opciones = [respuesta_correcta, respuesta_incorrecta_1, respuesta_incorrecta_2, respuesta_incorrecta_3]
-                random.shuffle(opciones)  # Mezclar las opciones de respuesta
-                for i, opcion in enumerate(opciones):
-                    botones_respuesta[i].config(text=opcion)
-            else:
-                label_pregunta.config(text="Fin del juego. Puntuación: " + str(puntuacion))
-                for boton in botones_respuesta:
-                    boton.config(state=tk.DISABLED)
-                    
-                    
+    def obtener_preguntas():
+        cursor.execute("SELECT preguntas, respuestas, respuestaErronea_uno, respuestaErronea_dos, respuestaErronea_tres FROM Preguntas")
+        preguntas_respuestas = cursor.fetchall()
+        random.shuffle(preguntas_respuestas)  # Mezclar las preguntas para que aparezcan en orden aleatorio
+        return preguntas_respuestas
 
-        def verificar_respuesta(opcion):
-            global pregunta_actual, puntuacion
-            pregunta, respuesta_correcta, _, _, _ = preguntas_respuestas[pregunta_actual]
-            if opcion == respuesta_correcta:
-                puntuacion += 100
-                mensaje = "¡Correcto!"
-            else:
-                mensaje = f"Incorrecto. La respuesta correcta es: {respuesta_correcta}"
+    preguntas_respuestas = obtener_preguntas()
 
-            pregunta_actual += 1
-            mostrar_pregunta()
-            if pregunta_actual < len(preguntas_respuestas):
-                messagebox.showinfo("Resultado", mensaje)  # Usa messagebox de esta manera
-            else:
-                messagebox.showinfo("Fin del juego", "¡Juego terminado! Tu puntuación: " + str(puntuacion))
-      
-                
+    def mostrar_pregunta():
+        global pregunta_actual
+        if pregunta_actual < len(preguntas_respuestas):
+            pregunta, respuesta_correcta, respuesta_incorrecta_1, respuesta_incorrecta_2, respuesta_incorrecta_3 = preguntas_respuestas[pregunta_actual]
+            label_pregunta.config(text=pregunta)
+            opciones = [respuesta_correcta, respuesta_incorrecta_1, respuesta_incorrecta_2, respuesta_incorrecta_3]
+            random.shuffle(opciones)  # Mezclar las opciones de respuesta
+            for i, opcion in enumerate(opciones):
+                botones_respuesta[i].config(text=opcion)
+        else:
+            label_pregunta.config(text="Fin del juego. Puntuación: " + str(puntuacion))
+            for boton in botones_respuesta:
+                boton.config(state=tk.DISABLED)
 
+    def verificar_respuesta(opcion):
+        global pregunta_actual, puntuacion
+        pregunta, respuesta_correcta, _, _, _ = preguntas_respuestas[pregunta_actual]
+        if opcion == respuesta_correcta:
+            puntuacion += 100
+            mensaje = "¡Correcto!"
+        else:
+            mensaje = f"Incorrecto. La respuesta correcta es: {respuesta_correcta}"
 
-        for i in range(4):
-            frame_grupo = tk.Frame(frame_respuestas, bg="#630551")
-            frame_grupo.pack(pady=30)
-            boton = tk.Button(frame_grupo, text="", font=("Helvetica", 17), command=lambda i=i: verificar_respuesta(botones_respuesta[i]['text']))
-            boton.pack(pady=10)
-
-            botones_respuesta.append(boton)
+        pregunta_actual += 1
         mostrar_pregunta()
-        button_salir=tk.Button(root, text="salir", command=lambda:root.destroy())
-        button_salir.pack(pady=12)
+        if pregunta_actual < len(preguntas_respuestas):
+            messagebox.showinfo("Resultado", mensaje)
+        else:
+            messagebox.showinfo("Fin del juego", "¡Juego terminado! Tu puntuación: " + str(puntuacion))
 
-            
+    for i in range(4):
+        frame_grupo = tk.Frame(frame_respuestas, bg="#630551")
+        frame_grupo.pack(pady=30)
+        boton = tk.Button(frame_grupo, text="", font=("Helvetica", 17), command=lambda i=i: verificar_respuesta(botones_respuesta[i]['text']))
+        boton.pack(pady=10)
 
-#quieor que este botton me deje salir de la pantalla de las preguntas
+        botones_respuesta.append(boton)
+    mostrar_pregunta()
+    button_salir = tk.Button(root, text="Salir", command=lambda: root.destroy())
+    button_salir.pack(pady=12)
 
-#funcion para salir de root
 root.mainloop()
-abrir_juego()
-
-# Iniciar el bucle principal de tkinter
-tk.mainloop()
